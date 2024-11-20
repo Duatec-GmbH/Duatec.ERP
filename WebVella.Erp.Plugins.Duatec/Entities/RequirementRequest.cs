1b596f157c078a77b84c9312370ab71191e4b1cd
Florian Reischl <florian.reischl@duatec.at>
2024-11-20 09:34:14 +0100

    added requirement request entities

---
+++ WebVella.Erp.Plugins.Duatec/Entities/RequirementRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebVella.Erp.Plugins.Duatec.Entities
+{
+    internal class RequirementRequest
+    {
+        public const string Entity = "requirement_request";
+        public const string Project = "project_id";
+        public const string Name = "name";
+    }
+}
---
+++ WebVella.Erp.Plugins.Duatec/Entities/RequirementRequestEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace WebVella.Erp.Plugins.Duatec.Entities
+{
+    internal class RequirementRequestEntry
+    {
+        public const string Entity = "requirement_request_entry";
+        public const string RequirementRequest = "requirement_request_id";
+        public const string Article = "article_id";
+        public const string Amount = "amount";
+    }
+}
