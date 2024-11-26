1ea63e307d7cdc3ed89272f36a7b3d83622e3c35
Florian Reischl <florian.reischl@duatec.at>
2024-11-26 09:36:23 +0100

    refactored delete part list delete hook

--- WebVella.Erp.Plugins.Duatec/Hooks/PartLists/PartListDeleteHook.cs
+++ WebVella.Erp.Plugins.Duatec/Hooks/PartLists/PartListDeleteHook.cs
@@ -4,6 +4,7 @@ using WebVella.Erp.Api.Models;
 using WebVella.Erp.Hooks;
 using WebVella.Erp.Plugins.Duatec.Entities;
 using WebVella.Erp.Plugins.Duatec.Persistance;
+using WebVella.Erp.Plugins.Duatec.Util;
 using WebVella.Erp.Web.Hooks;
 using WebVella.Erp.Web.Pages.Application;
 
@@ -16,17 +17,15 @@ namespace WebVella.Erp.Plugins.Duatec.Hooks.PartLists
         {
             var id = (Guid)pageModel.TryGetDataSourceProperty<EntityRecord>("Record")["id"];
 
-            var entries = PartListEntry.FindMany(id, "id")
-                .Select(r => (Guid)r["id"])
-                .ToArray();
-
             QueryResponse TransactionalAction()
             {
+                var entries = PartListEntry.FindMany(id, "id")
+                    .ToIdArray();
+
                 var recMan = new RecordManager();
                 recMan.DeleteRecords(PartListEntry.Entity, entries);
                 return recMan.DeleteRecord(PartList.Entity, id);
             }
-
             return Transactional.Delete(pageModel, TransactionalAction);
         }
     }
---
+++ WebVella.Erp.Plugins.Duatec/Util/ListEntityRecordExtensions.cs
@@ -0,0 +1,13 @@
+﻿using WebVella.Erp.Api.Models;
+
+namespace WebVella.Erp.Plugins.Duatec.Util
+{
+    internal static class ListEntityRecordExtensions
+    {
+        public static Guid[] ToIdArray(this List<EntityRecord> recList)
+        {
+            return recList.Select(r => (Guid)r["id"])
+                .ToArray();
+        }
+    }
+}
